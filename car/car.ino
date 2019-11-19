39f884717957d4129c3df49b64299cb108ff8b14
jelinekk <jelinekk@btlnet.com>
2019-11-19 17:48:44 +0100

    Refactoring, change of UUID last characters

--- car/car.ino
+++ car/car.ino
@@ -8,7 +8,10 @@
 #include <ArduinoBLE.h>
 
 #define DATA_LENGTH 6
-BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1218"); // create service
+#define UUID_car "19b10000-e8f2-537e-4f6c-d104768a1220"
+#define UUID_ctrl "19b10001-e8f2-537e-4f6c-d104768a1220"
+
+BLEService ledService(UUID_car); // create service
 
 // create switch characteristic and allow remote device to read and write
 BLEByteCharacteristic switchCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1215", BLERead | BLEWrite);
